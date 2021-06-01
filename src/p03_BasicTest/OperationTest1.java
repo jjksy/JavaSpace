edb69b5c4d01a523ddd3521caa3f7e43195f6ea4
jjksy <318thdus@naver.com>
2021-06-01 15:36:31 +0900

    OperationTest1

---
+++ src/p03_BasicTest/OperationTest1.java
@@ -0,0 +1,20 @@
+package p03_BasicTest;
+
+/*
+ * 산술연산자 테스트
+ */
+
+public class OperationTest1 {
+	public static void main(String[] args) {
+		int result = 0;
+		int var_int1 = 10;
+		int var_int2 = 2;
+
+		System.out.println("var_int1 + var_int2 = " + (var_int1 + var_int2));
+		System.out.println("var_int1 - var_int2 = " + (var_int1 - var_int2));
+		System.out.println("var_int1 * var_int2 = " + (var_int1 * var_int2));
+		System.out.println("var_int1 / var_int2 = " + (var_int1 / var_int2));
+		System.out.println("var_int1 % var_int2 = " + (var_int1 % var_int2));
+
+	}
+}
