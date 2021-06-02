81943fc60178fa9de0c3d11acab010955c9dec07
jjksy <318thdus@naver.com>
2021-06-02 09:05:42 +0900

    OperationTest7

---
+++ src/p03_BasicTest/OperationTest7.java
@@ -0,0 +1,20 @@
+package p03_BasicTest;
+
+//시프트 연산자 테스트
+
+public class OperationTest7 {
+	public static void main(String[] args) {
+		int x = 3;
+		int result = 0;
+
+		// <<
+		result = x<<2;
+		System.out.println("x<<2 =" + result);
+		// >>
+		result = x >>1;
+		System.out.println("x>>1=" + result);
+		// >>>
+		result = x>>>1;
+		System.out.println("x >>>1 = "+result);
+	}
+}
