34bd012d3bede003feeb96a98c27e5c82162b1ba
jjksy <318thdus@naver.com>
2021-06-03 23:04:56 +0900

    OverridingTest

---
+++ src/p05_Inheritance/OverridingTest.java
@@ -0,0 +1,12 @@
+package p05_Inheritance;
+//Overriding
+//자식클래스 에서 메소드를 재정의 한 경우
+
+class OverridingParent{
+	void parentMethod() {
+		System.out.println("parent Method");
+	}
+}
+public class OverridingTest {
+
+}
